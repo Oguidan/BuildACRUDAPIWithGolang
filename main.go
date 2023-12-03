07e0a3948531ced8f78046c5ba44a3c127d195e5
Michael Oguidan <michaeloguidan@gmail.com>
2023-12-03 18:46:54 +0000

    Add updateMovie function to update movie details

--- main.go
+++ main.go
@@ -61,6 +61,27 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movie)
 }
 
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	// Set json content type
+	w.Header().Set("Content-Type", "application/json")
+	// params
+	params := mux.Vars(r)
+	// loop over the movies range
+	// delete the movie with the i.d that you've sent
+	// add a new movie - the movie that we send in the body of postman
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 
